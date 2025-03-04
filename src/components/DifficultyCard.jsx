28dcaa20297fcb2577b073bd32790f8913a24442
Shrey <shrey17122003@gmail.com>
2025-03-04 17:56:13 +0530

    change difficulty-card className to btn

--- src/components/DifficultyCard.jsx
+++ src/components/DifficultyCard.jsx
@@ -12,13 +12,13 @@ export const DifficultyCard = ({ image, name, id, pairCount, time }) => {
   return (
     <button
       onClick={onClick}
-      className={`difficulty-card ${name} text-white bg-white/10 hover:bg-white/20 w-full aspect-[2/3] cursor-pointer relative z-0 group`}>
+      className={`btn ${name} text-white bg-white/5 hover:bg-black/5 transition-all w-full aspect-[2/3] cursor-pointer relative z-0 group`}>
       <div className="flex flex-col justify-center items-center w-full h-full">
         <p className="difficulty-name absolute top-[12%] text-lg md:text-xl lg:text-2xl 2xl:text-4xl capitalize font-bold mb-3">
           {name}
         </p>
         <img
-          className="difficulty-image w-6/12 max-w-36 transition-all duration-400 lg:grayscale-100 group-hover:grayscale-0"
+          className="difficulty-image w-6/12 max-w-36 transition-all duration-500 lg:grayscale-100 group-hover:grayscale-0"
           src={image}
           alt={name + "-image"}
         />
@@ -43,7 +43,7 @@ export const DifficultyCard = ({ image, name, id, pairCount, time }) => {
           </div>
         </div>
       </div>
-      <div className="difficulty-card-border overflow-clip">
+      <div className="btn-border overflow-clip">
         <div className="top main-border"></div>
         <div className="bottom main-border"></div>
         <div className="left main-border"></div>
