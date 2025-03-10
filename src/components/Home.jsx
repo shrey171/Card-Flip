41b9555de34d06e694dfd430682cc3ec1e43a8a7
Shrey <shrey17122003@gmail.com>
2025-03-10 13:06:15 +0530

    fix fouc

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,16 +1,27 @@
-import { useStore } from "hooks";
+import { useStore, useGrid } from "hooks";
 import { DifficultyCard } from "./DifficultyCard";
-import { useGrid } from "hooks";
+import { useEffect } from "react";
 
 export const Home = () => {
   const settings = useStore(state => state.settings);
   const preMadeSettings = settings.slice(0, 4);
   const { ref } = useGrid({ cellCount: 4, minWidth: 150 });
 
+  useEffect(() => {
+    const onLoad = () => {
+      ref.current.style.visibility = "visible";
+      // Preloading card's backface image
+      const img = new Image();
+      img.src = "/assets/images/test.jpg";
+    };
+    window.addEventListener("load", onLoad);
+    return () => window.removeEventListener("load", onLoad);
+  }, []);
+
   return (
     <div
       ref={ref}
-      style={{ height: "100vh" }}
+      style={{ height: "100vh", visibility: "hidden" }}
       className="gap-2 p-4 w-full 2xl:gap-4 xl:w-10/12 mx-auto place-content-center">
       {preMadeSettings.map(data => (
         <DifficultyCard key={data.id} {...data} />
