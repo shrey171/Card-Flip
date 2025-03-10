3fdee1960e4307930a79afe02c7f097f18d76f64
Shrey <shrey17122003@gmail.com>
2025-03-10 12:50:41 +0530

    make cards random again

--- src/hooks/useCardLogic.jsx
+++ src/hooks/useCardLogic.jsx
@@ -19,7 +19,7 @@ export const useCardLogic = () => {
         isFlipped: false,
         isMatched: false,
       }))
-      // .sort(() => Math.random() - 0.5);
+      .sort(() => Math.random() - 0.5);
   };
 
   const handleFlip = card => {
